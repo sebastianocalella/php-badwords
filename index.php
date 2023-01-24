c18ca7463abce06711d1c9764e5534ade7302194
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-24 15:48:37 +0100

    first php file creation and requests

---
+++ index.php
@@ -0,0 +1,8 @@
+<label for="paragraph">Paragrafo:</label>
+
+<textarea id="paragraph" name="story" rows="5" cols="30" placeholder="Scrivi qui il tuo paragrafo..."></textarea>
+<input type="text" placeholder="Scrivi qui la parola da censurare">
+
+<form action="">
+
+</form>
